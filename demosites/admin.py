a42e6b66b52e303f7f745ed02dc96ad74b3096a5
Moin Ul Haq <150945042+moin-ul-haq@users.noreply.github.com>
2026-08-01 04:49:30 +0500

    Register GeneratedSite + SiteInquiry in Django admin

--- demosites/admin.py
+++ demosites/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import GeneratedSite, SiteInquiry
+
+
+@admin.register(GeneratedSite)
+class GeneratedSiteAdmin(admin.ModelAdmin):
+    list_display = ("business_name", "slug", "niche", "city", "user", "color_scheme", "tone", "created_at")
+    search_fields = ("business_name", "slug", "city")
+    list_filter = ("color_scheme", "tone")
+
+
+@admin.register(SiteInquiry)
+class SiteInquiryAdmin(admin.ModelAdmin):
+    list_display = ("name", "site", "phone", "email", "created_at")
+    search_fields = ("name", "email", "phone", "message")
+    list_filter = ("site",)
+    readonly_fields = ("site", "name", "email", "phone", "message", "created_at")
